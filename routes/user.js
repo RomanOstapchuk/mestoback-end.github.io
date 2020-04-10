008decc6e304d8e56e94353514b534e55dd61af4
Roman Ostapchuk <pamwdp@yandex.ru>
2020-04-10 21:55:39 +0300

    v0.0.3

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,31 @@
 const router = require('express').Router();
-const users  = require('../data/user');
+const path = require('path');
+const fs = require('fs');
+
+const dirusers = path.join(__dirname, '../data/user.json');
+const users = require('../data/user');
 
 router.get('/users/:id', (req, res) => {
-  if (!users.find(item => item._id === req.params.id)) {
-    res.status(404).send({"message": "Нет пользователя с таким id"});
-    return;
-  }else{
-    res.send(users.find(item => item._id === req.params.id));
-  };
+  fs.readFile(dirusers, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log('Users is not found');
+    }
+    if (!users.find(item => item._id === req.params.id)) {
+      res.status(404).send({ 'message': 'Нет пользователя с таким id' });
+    } else {
+      res.send(users.find(item => item._id === req.params.id));
+    }
+  });
 });
 
 router.get('/users', (req, res) => {
-  res.send(users);
+  fs.readFile(dirusers, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log('Users is not found');
+      return;
+    }
+    res.send(users);
+  });
 });
 
 module.exports = router;
